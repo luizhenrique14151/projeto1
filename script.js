e6df8a1d714ed4fb3aa8ae61f2c8de627951b8fb
luizhenrique14151 <107251715+luizhenrique14151@users.noreply.github.com>
2023-08-23 11:09:26 -0300

    issoaqui

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.getElementById("colorButton").addEventListener("click", function() {
-    document.body.classList.toggle("colorChange");
-  });
+var cores = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff', '#ff9900', '#9900ff', '#0099ff', '#ff0099'];
+function mudarCor() {
+    var corAleatoria = cores[Math.floor(Math.random() * cores.length)];
+    document.body.style.backgroundColor = corAleatoria;
+}
